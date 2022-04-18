cfff9052404d375af1d0f0cd30331b923c3b2229
Zohorl <zohorul109561@gmail.com>
2022-04-18 16:22:31 +0600

    footer logo added

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from '../../../Images/logo1.png';
+import './Footer.css';
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear()
     return (
         <div>
-            <p><small>Copyrights &copy; {year} All Rights Reserved , Powered By Health Coach</small></p>
+            <p><small>Copyrights &copy; {year} All Rights Reserved , Powered By <span><img id='footer-logo' src={logo} alt="" /></span> </small></p>
         </div>
     );
 };
